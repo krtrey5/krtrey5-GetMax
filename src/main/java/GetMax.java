4120a36ca5470644a35601a4e5676652ea585c25
krtrey5 <krtrey@outlook.com>
2023-02-28 01:40:32 +0000

    Max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -11,6 +11,7 @@ public class GetMax {
         for(int i = 0; i < arr.length; i++){
             if(arr[i] > num){
                 num = arr[i];
+                //test
             }
         }
         return num;
